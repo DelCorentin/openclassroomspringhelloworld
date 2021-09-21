6e8e38bbe387a078a3f174364b0135b18bcef113
DelCorentin <corentin.delvi@gmail.com>
2021-09-21 14:57:16 +0200

    Initial commit

---
+++ src/main/java/com/openclassroom/helloworld/HelloworldApplication.java
@@ -0,0 +1,15 @@
+package com.openclassroom.helloworld;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(HelloworldApplication.class, args);
+
+	}
+}
---
+++ src/main/java/com/openclassroom/helloworld/Run.java
@@ -0,0 +1,13 @@
+package com.openclassroom.helloworld;
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Run implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Hello my World !");
+    }
+}
